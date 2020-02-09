93de33e911ee2c1055dcf8e9c38703bc91869ad2
koreisis <pornoromen0101@gmail.com>
2020-02-10 01:19:56 +0900

    CV0.9809

--- src/augment.py
+++ src/augment.py
@@ -13,11 +13,11 @@ augmix_transform = [
 train_transform = Compose([
     ShiftScaleRotate(rotate_limit=15, p=0.5),
     # GaussNoise(p=0.2),
-    OneOf([
-        OpticalDistortion(p=1),
-        GridDistortion(p=1),
-        # ElasticTransform(p=1),
-    ], p=0.2),
+    # OneOf([
+    #     OpticalDistortion(p=1),
+    #     GridDistortion(p=1),
+    #     # ElasticTransform(p=1),
+    # ], p=0.2),
     # Cutout(max_h_size=8, max_w_size=8, p=0.2),
 ])
 
